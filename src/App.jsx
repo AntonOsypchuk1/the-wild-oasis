5b280c7740466220a2daf4a48a1e35a480328358
Anton Osypchuk <osypchuk.ua@gmail.com>
2023-11-03 15:31:40 +0200

    checking out styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
+import Heading from "./ui/Heading.jsx";
+import Row from "./ui/Row.jsx";
+
+const StyledApp = styled.main`
+  padding: 20px;
+`;
 
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+
+              <Button onClick={() => {}}>Check in</Button>
+              <Button variation="second" size="small" onClick={() => {}}>
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Forms</Heading>
+
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 };
 
 export default App;
